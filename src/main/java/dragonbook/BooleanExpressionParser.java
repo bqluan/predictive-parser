4c6ba91813cebd460b6b61ff1bf3ce8d0c11c759
Bing-Qian Luan <bingqian.luan@gmail.com>
2012-12-04 11:27:58 +0800

    Parse boolean expression to reverse polish notation

--- src/main/java/dragonbook/BooleanExpressionParser.java
+++ src/main/java/dragonbook/BooleanExpressionParser.java
@@ -1,15 +1,21 @@
 package dragonbook;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class BooleanExpressionParser {
   private String expr;
   private int index;
   private char lookahead;
+  private List<String> reversePolishNotation;
 
-  public void parse(String expression) throws SyntaxException {
+  public List<String> parse(String expression) throws SyntaxException {
     expr = expression == null || "".equals(expression) ? "" : expression;
     index = 0;
     lookahead = index == expr.length() ? '\0' : expr.charAt(index);
+    reversePolishNotation = new ArrayList<String>();
     expr();
+    return reversePolishNotation;
   }
 
   private void expr() throws SyntaxException {
@@ -19,9 +25,12 @@ public class BooleanExpressionParser {
 
   private void comp() throws SyntaxException {
     if (Character.isLetter(lookahead)) {
-      key();
-      op();
-      value();
+      String key = key();
+      String op = op();
+      String value = value();
+      reversePolishNotation.add(key);
+      reversePolishNotation.add(value);
+      reversePolishNotation.add(op);
     } else if ('(' == lookahead) {
       match('(');
       expr();
@@ -37,22 +46,25 @@ public class BooleanExpressionParser {
         match('&');
         match('&');
         comp();
+        reversePolishNotation.add("&&");
         expr_();
         break;
       case '|':
         match('|');
         match('|');
         comp();
+        reversePolishNotation.add("||");
         expr_();
         break;
       case '\0':
+      case ')':
         break;
       default:
-        throw new SyntaxException("'&' '|' '\\0' expected", expr, index);
+        throw new SyntaxException("'&' '|' ')' '\\0' expected", expr, index);
     }
   }
 
-  private void key() throws SyntaxException {
+  private String key() throws SyntaxException {
     if (!Character.isLetter(lookahead)) {
       throw new SyntaxException("letter expected", expr, index);
     }
@@ -65,45 +77,74 @@ public class BooleanExpressionParser {
       key.append(lookahead);
       match(lookahead);
     }
+
+    return key.toString();
   }
 
-  private void op() throws SyntaxException {
+  private String op() throws SyntaxException {
+    String op = null;
+
     switch (lookahead) {
       case '=':
         match('=');
         match('=');
+        op = "==";
         break;
       case '!':
         match('!');
         match('=');
+        op = "!=";
         break;
       case '<':
         match('<');
         if (lookahead == '=') {
           match('=');
+          op = "<=";
+        } else {
+          op = "<";
         }
         break;
       case '>':
         match('>');
         if (lookahead == '=') {
           match('=');
+          op = ">=";
+        } else {
+          op = ">";
         }
+        break;
+      case '_':
+        match('_');
+        match('_');
+        match('i');
+        match('n');
+        match('_');
+        match('_');
+        op = "__in__";
         break;
       default:
         throw new SyntaxException("logic operator expected", expr, index);
     }
+
+    return op;
   }
 
-  private void value() throws SyntaxException {
+  private String value() throws SyntaxException {
     if (lookahead != '\'') {
       throw new SyntaxException("'\'' expected", expr, index);
     }
 
+    StringBuilder value = new StringBuilder();
+    value.append('\'');
     match('\'');
     while (lookahead != '\'') {
+      value.append(lookahead);
       match(lookahead);
     }
+    value.append('\'');
     match('\'');
+
+    return value.toString();
   }
 
   private void match(char c) throws SyntaxException {
--- src/test/java/dragonbook/BooleanExpressionParserTest.java
+++ src/test/java/dragonbook/BooleanExpressionParserTest.java
@@ -1,5 +1,7 @@
 package dragonbook;
 
+import java.util.List;
+
 import junit.framework.TestCase;
 
 public class BooleanExpressionParserTest extends TestCase {
@@ -7,11 +9,6 @@ public class BooleanExpressionParserTest extends TestCase {
 
   public void setUp() {
     parser = new BooleanExpressionParser();
-  }
-
-  public void testParse() throws SyntaxException {
-    parser.parse("name=='bob'");
-    parser.parse("a>='10'&&a<='1000'");
   }
 
   public void testParseEmpty() {
@@ -28,5 +25,109 @@ public class BooleanExpressionParserTest extends TestCase {
       fail("SyntaxException expected");
     } catch (SyntaxException e) {
     }
+  }
+
+  public void testParseEqual() throws SyntaxException {
+    List<String> reversePolishNotation = parser.parse("name=='bob'");
+    assertEquals(3, reversePolishNotation.size());
+    assertEquals("name", reversePolishNotation.get(0));
+    assertEquals("'bob'", reversePolishNotation.get(1));
+    assertEquals("==", reversePolishNotation.get(2));
+  }
+
+  public void testParseNotEqual() throws SyntaxException {
+    List<String> reversePolishNotation = parser.parse("name!='bob'");
+    assertEquals(3, reversePolishNotation.size());
+    assertEquals("name", reversePolishNotation.get(0));
+    assertEquals("'bob'", reversePolishNotation.get(1));
+    assertEquals("!=", reversePolishNotation.get(2));
+  }
+
+  public void testParseIn() throws SyntaxException {
+    List<String> reversePolishNotation = parser.parse("name__in__'bob'");
+    assertEquals(3, reversePolishNotation.size());
+    assertEquals("name", reversePolishNotation.get(0));
+    assertEquals("'bob'", reversePolishNotation.get(1));
+    assertEquals("__in__", reversePolishNotation.get(2));
+  }
+
+  public void testParseGreaterThan() throws SyntaxException {
+    List<String> reversePolishNotation = parser.parse("name>'bob'");
+    assertEquals(3, reversePolishNotation.size());
+    assertEquals("name", reversePolishNotation.get(0));
+    assertEquals("'bob'", reversePolishNotation.get(1));
+    assertEquals(">", reversePolishNotation.get(2));
+  }
+
+  public void testParseLessThan() throws SyntaxException {
+    List<String> reversePolishNotation = parser.parse("name<'bob'");
+    assertEquals(3, reversePolishNotation.size());
+    assertEquals("name", reversePolishNotation.get(0));
+    assertEquals("'bob'", reversePolishNotation.get(1));
+    assertEquals("<", reversePolishNotation.get(2));
+  }
+
+  public void testParseGreaterThanOrEqualTo() throws SyntaxException {
+    List<String> reversePolishNotation = parser.parse("name>='bob'");
+    assertEquals(3, reversePolishNotation.size());
+    assertEquals("name", reversePolishNotation.get(0));
+    assertEquals("'bob'", reversePolishNotation.get(1));
+    assertEquals(">=", reversePolishNotation.get(2));
+  }
+
+  public void testParseLessThanOrEqualTo() throws SyntaxException {
+    List<String> reversePolishNotation = parser.parse("name<='bob'");
+    assertEquals(3, reversePolishNotation.size());
+    assertEquals("name", reversePolishNotation.get(0));
+    assertEquals("'bob'", reversePolishNotation.get(1));
+    assertEquals("<=", reversePolishNotation.get(2));
+  }
+
+  public void testParseGroup() throws SyntaxException {
+    List<String> reversePolishNotation = parser.parse("(name<='bob')");
+    assertEquals(3, reversePolishNotation.size());
+    assertEquals("name", reversePolishNotation.get(0));
+    assertEquals("'bob'", reversePolishNotation.get(1));
+    assertEquals("<=", reversePolishNotation.get(2));
+  }
+
+  public void testParsePostfixGroup() throws SyntaxException {
+    List<String> reversePolishNotation = parser.parse("a=='a'&&(b=='b'||c=='c')");
+    assertEquals(11, reversePolishNotation.size());
+    assertEquals("a", reversePolishNotation.get(0));
+    assertEquals("'a'", reversePolishNotation.get(1));
+    assertEquals("==", reversePolishNotation.get(2));
+    assertEquals("b", reversePolishNotation.get(3));
+    assertEquals("'b'", reversePolishNotation.get(4));
+    assertEquals("==", reversePolishNotation.get(5));
+    assertEquals("c", reversePolishNotation.get(6));
+    assertEquals("'c'", reversePolishNotation.get(7));
+    assertEquals("==", reversePolishNotation.get(8));
+    assertEquals("||", reversePolishNotation.get(9));
+    assertEquals("&&", reversePolishNotation.get(10));
+  }
+
+  public void testParseAnd() throws SyntaxException {
+    List<String> reversePolishNotation = parser.parse("a>='10'&&a<='20'");
+    assertEquals(7, reversePolishNotation.size());
+    assertEquals("a", reversePolishNotation.get(0));
+    assertEquals("'10'", reversePolishNotation.get(1));
+    assertEquals(">=", reversePolishNotation.get(2));
+    assertEquals("a", reversePolishNotation.get(3));
+    assertEquals("'20'", reversePolishNotation.get(4));
+    assertEquals("<=", reversePolishNotation.get(5));
+    assertEquals("&&", reversePolishNotation.get(6));
+  }
+
+  public void testParseOr() throws SyntaxException {
+    List<String> reversePolishNotation = parser.parse("a>='10'||a<='20'");
+    assertEquals(7, reversePolishNotation.size());
+    assertEquals("a", reversePolishNotation.get(0));
+    assertEquals("'10'", reversePolishNotation.get(1));
+    assertEquals(">=", reversePolishNotation.get(2));
+    assertEquals("a", reversePolishNotation.get(3));
+    assertEquals("'20'", reversePolishNotation.get(4));
+    assertEquals("<=", reversePolishNotation.get(5));
+    assertEquals("||", reversePolishNotation.get(6));
   }
 }
